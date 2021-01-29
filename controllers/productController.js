8edecbca8bc70dc2cbbbb023083e2c274f027605
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-01-29 14:13:16 +0200

    details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,4 +9,8 @@ router.get('/create', (req, res) => {
     res.render('create', {title: 'Create'});
 });
 
+router.get ('/details/:productId', (req, res) => {
+    res.render('details', {title: 'Details'});
+})
+
 module.exports = router;
