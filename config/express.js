d9c98f582f38170cd134a1f4cc84603cf01367ea
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-01-29 16:22:20 +0200

    bodyparser

--- config/express.js
+++ config/express.js
@@ -6,10 +6,14 @@ module.exports = (app) => {
     app.engine('hbs', handlebars({
         extname: 'hbs',
     }));
-    
+
     app.set('view engine', 'hbs');
-    
-    app.use(express.static('public'))
+
+    app.use(express.static('public'));
+
+    app.use(express.urlencoded({
+        extended: true
+    }));
     //TODO: Setup the view engine
 
     //TODO: Setup the body parser
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,31 @@
 const { Router } = require('express');
+const uniqid = require('uniqid');
+const Cube = require('../models/cubeModel');
+
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Browse'});
+    res.render('home', { title: 'Browse' });
 });
 
 router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create'});
+    res.render('create', { title: 'Create' });
 });
 
-router.get ('/details/:productId', (req, res) => {
-    res.render('details', {title: 'Details'});
+router.post('/crete', (req, res) => {
+    let data = req.body;
+    let cube = new Cube(uniqid(),
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficultyLevel
+    );
+
+    res.redirect('/products');
+});
+
+router.get('/details/:productId', (req, res) => {
+    res.render('details', { title: 'Details' });
 })
 
 module.exports = router;
