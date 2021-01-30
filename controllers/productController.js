96b925e0d92b5887e14af27e47c4c8bfa0b48101
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-01-30 11:56:28 +0200

    cubes

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,8 @@ const productService = require('../services/productService')
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home', { title: 'Browse' });
+    let products = productService.getAll();
+    res.render('home', { title: 'Browse', products });
 });
 
 router.get('/create', (req, res) => {
@@ -12,7 +13,7 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-    
+
     productService.create(req.body)
 
     res.redirect('/products');
