0ca02e8f76486831085010417c3f57eeb8a75474
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-02-01 22:00:12 +0200

    changes

--- models/cubeModel.js
+++ models/cubeModel.js
@@ -1,16 +1,39 @@
-const productsDb = require('../config/database.json');
+//const productsDb = require('../config/database.json');
 const Model = require('./Model');
+
+
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 50,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/,
+    },
+    difficultyLeve: {
+        type: Number
+    },
+});
 
 class Cube extends Model {
     constructor(id, name, description, imageUrl, level) {
-        super() 
-            this.id = id;
-            this.name = name;
-            this.description = description;
-            this.imageUrl = imageUrl;
-            this.level = level;
-        }
-    
+        super()
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.level = level;
+    }
+
 }
 
 
