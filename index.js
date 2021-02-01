5ae5074f35547975defef3433054403617954ea4
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-02-01 12:49:01 +0200

    mongoose config

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 
 require('./config/express')(app);
+require('./config/mongoose')(app);
 
 
 app.use(routes)
--- models/cubeModel.js
+++ models/cubeModel.js
@@ -2,13 +2,15 @@ const productsDb = require('../config/database.json');
 const Model = require('./Model');
 
 class Cube extends Model {
-    super(id, name, description, imageUrl, level) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.level = level;
-    }
+    constructor(id, name, description, imageUrl, level) {
+        super() 
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.imageUrl = imageUrl;
+            this.level = level;
+        }
+    
 }
 
 
