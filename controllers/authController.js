c7c725680b96748b49c732e2516a63b19def5b9a
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-02-18 07:58:02 +0200

    logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,20 +3,23 @@ const { Router } = require('express');
 const router = Router();
 
 const authService = require('../services/authService');
+const isAuthenticated = require('../middlewares/isAuthenticated');
+
+const isGuest = require('../middlewares/isGuest');
 
 const { COOKIE_NAME } = require('../config/config')
 
-router.get('/login', (req, res) => {
+router.get('/login', isGuest, (req, res) => {
     res.render('login');
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest, async (req, res) => {
     const { username, password } = req.body;
 
     try {
         let token = await authService.login({ username, password });
 
-        res.cookie(COOKIE_NAME , token);
+        res.cookie(COOKIE_NAME, token);
         res.redirect('/products');
     } catch (error) {
         console.log(console.error);
@@ -24,11 +27,11 @@ router.post('/login', async (req, res) => {
     }
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', isGuest, (req, res) => {
     res.render('register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', isGuest, async (req, res) => {
     const { username, password, repeatPassword } = req.body
 
     if (password !== repeatPassword) {
@@ -45,6 +48,11 @@ router.post('/register', async (req, res) => {
         res.render('register', { error });
     }
 
+});
+
+router.get('/logout',isAuthenticated, (req, res) => {
+    res.clearCookie(COOKIE_NAME);
+    res.redirect('/products');
 });
 
 module.exports = router;
