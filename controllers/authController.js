b77364f1b55de6c7895bf1bce4c689fb615ce889
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-02-20 14:13:51 +0200

    validations

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const validator = require('validator');
+const { body, validationResult } = require('express-validator');
 
 const authService = require('../services/authService');
 const isAuthenticated = require('../middlewares/isAuthenticated');
@@ -31,8 +32,8 @@ router.get('/register', isGuest, (req, res) => {
     res.render('register');
 });
 
-router.post('/register', isGuest, async (req, res) => {
-    const { username, password, repeatPassword } = req.body
+const isStrongPasswordMiddleware = (req, res, next) => {
+    let password = req.body.password;
 
     let isStrongPassword = validator.isStrongPassword(password, {
         minLength: 8,
@@ -42,26 +43,40 @@ router.post('/register', isGuest, async (req, res) => {
         minSymbols: 1,
     });
 
-
-    if (password !== repeatPassword) {
-        res.render('register', { message: 'Password missmatch!' })
-        return;
+    if (!isStrongPassword) {
+        return res.render('register', { error: { message: 'You should have strong password!' }, username: req.body });
     }
+    next();
+}
 
-    try {
-        let user = await authService.register({ username, password });
+router.post('/register',
+    isGuest,
+    //isStrongPasswordMiddleware,
+    body('username', 'Specify username').notEmpty(),
+    body('password', 'Password too short').isLength({ min: 5 }),
+    async (req, res) => {
+        const { username, password, repeatPassword } = req.body
 
-        if (!isStrongPassword) {
-            throw { message: 'You should have strong password!' };
+        if (password !== repeatPassword) {
+            res.render('register', { message: 'Password missmatch!' });
+            return;
         }
 
-        res.redirect('auth/login');
+        let errors = validationResult(req);
+        if (errors.errors.length > 0) {
+            return res.render('register', errors );
+        }
 
-    } catch (error) {
-        res.render('register', { error });
-    }
+        try {
+            let user = await authService.register({ username, password });
 
-});
+            res.redirect('/auth/login');
+
+        } catch (error) {
+            res.render('register', { error });
+        }
+
+    });
 
 router.get('/logout', isAuthenticated, (req, res) => {
     res.clearCookie(COOKIE_NAME);
