e42b75956cc0d641f7a9beb2cb6990c3ee5af7b0
hopenvy <73658880+hopenvy@users.noreply.github.com>
2021-02-20 09:46:30 +0200

    password properties

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-
 const router = Router();
+const validator = require('validator');
 
 const authService = require('../services/authService');
 const isAuthenticated = require('../middlewares/isAuthenticated');
-
 const isGuest = require('../middlewares/isGuest');
+
 
 const { COOKIE_NAME } = require('../config/config')
 
@@ -34,6 +34,15 @@ router.get('/register', isGuest, (req, res) => {
 router.post('/register', isGuest, async (req, res) => {
     const { username, password, repeatPassword } = req.body
 
+    let isStrongPassword = validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+    });
+
+
     if (password !== repeatPassword) {
         res.render('register', { message: 'Password missmatch!' })
         return;
@@ -41,6 +50,10 @@ router.post('/register', isGuest, async (req, res) => {
 
     try {
         let user = await authService.register({ username, password });
+
+        if (!isStrongPassword) {
+            throw { message: 'You should have strong password!' };
+        }
 
         res.redirect('auth/login');
 
@@ -50,7 +63,7 @@ router.post('/register', isGuest, async (req, res) => {
 
 });
 
-router.get('/logout',isAuthenticated, (req, res) => {
+router.get('/logout', isAuthenticated, (req, res) => {
     res.clearCookie(COOKIE_NAME);
     res.redirect('/products');
 });
